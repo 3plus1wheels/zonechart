efbab54da5101a8d494fd80315013e2025c65ae9
3plus1wheels <3plus1wheels@gmail.com>
2025-06-06 17:22:43 -0600

    setup venv

--- scripts/main.py
+++ scripts/main.py
@@ -14,6 +14,7 @@ def get_employeeDict():
         #print(schedule.to_string())
 
         schedule[8] = schedule[8].replace(np.nan, '0')
+        #/Users/derek/Documents/vova/zone chart/zonechart/data/employee test.xlsx
 
         for  i in range(len(schedule)):
             if schedule.iloc[i, 8] != '0' and schedule.iloc[i, 8] != "Employee" and schedule.iloc[i, 8] not in employeeDict:
