bc8f554d3f72aed0b819823f8de5c1355ba45295
3plus1wheels <3plus1wheels@gmail.com>
2025-06-07 00:11:37 -0600

    latest

--- scripts/employees.py
+++ scripts/employees.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+from docx import Document
 
 class employee:
-    def __init__(self, name, position, mens, womens, cash, fits, stock, greet):
+    def __init__(self, name, position, mens, womens, cash, fits, stock, greet, replen, sfs):
         self.name = name
         self.position = position
         self.mens = mens
@@ -11,11 +12,24 @@ class employee:
         self.fits = fits
         self.stock = stock
         self.greet = greet
+        self.replen = replen
+        self.sfs = sfs
         self.info = self.get_info()
 
-    skillRaw = pd.read_excel("data/employee test.xlsx", header = 0)
-    skill = skillRaw.copy()
-    rows, cols = skill.shape
+    doc = Document("data/EMPLOYEE  KNOWLEDGE BASE NEW.docx")
+    for table in doc.tables:
+        for row in table.rows:
+            for cell in row.cells:
+                print(cell.text, end="\t")
+            print()
+
+    table = doc.tables[0]
+
+    
+
+
+    print(table.cell(1, 0).text)  # Example to access the first cell of the first table
+    
 
     def get_info(self):
         return f"Employee Name: {self.name}, Position: {self.position}, Mens: {self.mens}, Womens: {self.womens}, Cash: {self.cash}, Fits: {self.fits}, Stock: {self.stock}, Greet: {self.greet}"
